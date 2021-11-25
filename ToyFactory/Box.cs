88c03f8a415ee8b16c668b54d2166b63bcaee35a
EsKill01 <edbarboza20@gmail.com>
2021-11-25 09:51:35 -0600

    Add project files.

---
+++ ToyFactory/Box.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToyFactory
+{
+    public class Box<T>
+    {
+
+        public T Content { get; private set; }
+
+        private bool hasContent;
+
+        public void Put(T content)
+        {
+            if (hasContent)
+            {
+                throw new Exception("Ya hay algo en la caja");
+
+            }
+
+            this.Content = content;
+
+            hasContent = true;
+        }
+
+        public T Extract()
+        {
+            if (!hasContent)
+            {
+                throw new Exception("No hay nada en la caja para extraer");
+            }
+
+            var content = this.Content;
+            this.Content = default(T);
+            hasContent = false;
+            return content;
+
+        }
+
+    }
+}
---
+++ ToyFactory/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ToyFactory
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var doll = new Doll();
+            var box = new Box<Doll>();
+            var box2 = new Box<Carr>();
+
+
+            box.Put(doll);
+            box2.Put(new Carr());
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
